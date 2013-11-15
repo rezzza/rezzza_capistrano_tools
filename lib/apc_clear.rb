a3cf98a1aac961f33bf8e2a9e23240183f1c88c6
Stéphane PY <py.stephane1@gmail.com>
2013-11-15 16:13:44 +0100

    Fixing tab.

--- lib/apc_clear.rb
+++ lib/apc_clear.rb
@@ -7,13 +7,13 @@ set :apc_clear_security_pass, nil
 before "deploy:create_symlink" do
     if (apc_keys_clear.kind_of?(Array) == true && apc_keys_clear.count > 0)
         case apc_clear_security
-            when "none"
-                options = nil;
-            when "http_basic"
-                options = "--user #{apc_clear_security_user}:#{apc_clear_security_pass}"
-            else
-                raise "apc_clear_security must be “none“ or “http_basic“"
-            end
+        when "none"
+            options = nil;
+        when "http_basic"
+            options = "--user #{apc_clear_security_user}:#{apc_clear_security_pass}"
+        else
+            raise "apc_clear_security must be “none“ or “http_basic“"
+        end
 
         capifony_pretty_print("Clearing APC cache.")
 
